5e779d543b07c7bed215705817a2b313c42e3926
Alan Tucker <alan.tucker@gmail.com>
2024-07-05 16:11:48 +0100

    Add new Ticket model
    A new model named Ticket has been created in the Models directory. This model includes a relationship to the User model, identifying the user that owns the ticket.

---
+++ app/Models/Ticket.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Ticket extends Model
+{
+    use HasFactory;
+
+
+    /**
+     * Get the user that owns the ticket.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+}
