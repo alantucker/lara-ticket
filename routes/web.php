30e0b62c3c85e3d293e5590fce79293a36504a35
Alan Tucker <alan.tucker@gmail.com>
2024-07-07 18:35:04 +0100

    Update dashboard route to use UserDashboard Controller
    The '/dashboard' route previously returned a simple view. This has been updated to now use the UserDashboard controller, moving this within the 'auth' middleware group for improved organization and security. This change enhances the route's functionality and maintains user authentication protection.

--- routes/web.php
+++ routes/web.php
@@ -2,23 +2,24 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TicketController;
+use App\Http\Controllers\UserDashboard;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
+
+    Route::get('/dashboard', UserDashboard::class)->name('dashboard');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::get('/tickets', [TicketController::class, 'index'])->name('ticket.index');
     Route::post('/tickets', [TicketController::class, 'store'])->name('ticket.store');
+
 });
 
 require __DIR__.'/auth.php';
