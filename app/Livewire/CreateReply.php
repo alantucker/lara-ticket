7165d17dca86e305693f472f06ec8acfc37c8766
Alan Tucker <alan.tucker@gmail.com>
2024-07-08 15:01:05 +0100

    Add CreateReply Livewire component
    This commit introduces a new Livewire component, CreateReply. This component handles reply creation including validation, saving replies to the database, and redirecting the user back to the ticket view page. It also provides a special view for creating replies.

---
+++ app/Livewire/CreateReply.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Reply;
+use App\Models\Ticket;
+use Livewire\Component;
+
+class CreateReply extends Component
+{
+    public $ticketId = '';
+    public $body = '';
+
+
+    /**
+     * Retrieve the custom error messages for validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'body.required' => 'A reply is required',
+        ];
+    }
+
+
+    /**
+     * Save the reply to the database and redirect to the ticket view page.
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function save()
+    {
+        $this->validate([
+            'body' => 'required',
+        ]);
+
+        $reply = new Reply([
+            'body' => $this->body,
+            'user_id' => auth()->id(),
+        ]);
+
+        $ticket = Ticket::find($this->ticketId);
+
+        $ticket->replies()->save($reply);
+
+        session()->flash('status', 'Reply successfully added and email sent to the user.');
+
+        return redirect()->to('/tickets/' . $this->ticketId);
+    }
+
+
+    /**
+     * Render the view for the create-reply component.
+     *
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function render()
+    {
+        return view('livewire.create-reply');
+    }
+}
