7be438a206fd6aca6ac16e2028e9eebeebb43750
Alan Tucker <alan.tucker@gmail.com>
2024-07-08 19:08:16 +0100

    Add ReplyAdded notification class
    This commit introduces a new notification class, ReplyAdded. The class is responsible for sending emails when a new reply is added to a ticket. It constructs an email message with pertinent ticket information and a link to view the ticket and its replies.

---
+++ app/Notifications/ReplyAdded.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Reply;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ReplyAdded extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(public $data)
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->subject('New reply added to Ticket #' . $this->data['ticket_id'])
+            ->greeting('Hey ' . $this->data['first_name'])
+            ->line('A new reply has been added to your ticket.')
+            ->line('Ticket Subject: ' . $this->data['ticket_subject'])
+            ->line($this->data['reply_body'])
+            ->action('View Ticket & Replies', url(config('app.url')) . '/tickets/' . $this->data['ticket_id'])
+            ->line('Thank you for using ' . config('app.name'));
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
