64ef4e8c5698b5f7201dddcb748714c7314282d9
Alan Tucker <alan.tucker@gmail.com>
2024-07-09 13:45:28 +0100

    Change "New Ticket" button to link in dashboard view
    The button for creating new tickets in the dashboard view was changed to a link.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,7 +15,7 @@
                                 <h2 class="text-xl font-semibold leading-6 text-gray-900">My Open Tickets</h2>
                             </div>
                             <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-                                <button type="button" class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">New Ticket</button>
+                                <a href="{{ route('ticket.create') }}" class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">New Ticket</a>
                             </div>
                         </div>
                         <div class="-mx-4 mt-8 sm:-mx-0">
