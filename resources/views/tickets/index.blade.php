03b13ac68131e5f073b8fb4bbeea965217d149c3
Alan Tucker <alan.tucker@gmail.com>
2024-07-09 13:27:38 +0100

    Update ticket index view layout
    The ticket index view has been updated to use the app layout. The actual content is set as a 'TODO' for future development.

--- resources/views/tickets/index.blade.php
+++ resources/views/tickets/index.blade.php
@@ -1,1 +1,18 @@
-<h1>Tickets Index</h1>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Tickets') }}
+        </h2>
+    </x-slot>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <div class="px-4 sm:px-6 lg:px-8">
+                        <p>TODO</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
