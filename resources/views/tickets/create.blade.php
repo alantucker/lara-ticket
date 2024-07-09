599a2baa411ed8fcbf9f3b9ff71236af173192de
Alan Tucker <alan.tucker@gmail.com>
2024-07-09 17:43:51 +0100

    Update category field in ticket creation view
    Updated the "category" field to "category_id" in the ticket creation form to align with the database column name. This ensures proper binding and validation in the form.

--- resources/views/tickets/create.blade.php
+++ resources/views/tickets/create.blade.php
@@ -25,11 +25,11 @@
                                 <textarea id="body" name="body" rows="3" class="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6">{{ old('body') }}</textarea>
                             </div>
                             <div class="col-span-6 sm:col-span-3">
-                                <label for="category" class="block text-sm font-medium leading-6 text-gray-900">Category <span class="text-xs text-red-500">* @error('category') Required @enderror</span></label>
-                                <select id="category" name="category" class="mt-2 block w-full rounded-md border-0 bg-white py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                <label for="category_id" class="block text-sm font-medium leading-6 text-gray-900">Category <span class="text-xs text-red-500">* @error('category') Required @enderror</span></label>
+                                <select id="category_id" name="category_id" class="mt-2 block w-full rounded-md border-0 bg-white py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                                     <option value="">Select</option>
                                     @foreach($categories as $category)
-                                        <option value="{{ $category->id }}" {{ (old('category') == $category->id) ? 'selected' : '' }}>{{ $category->name }}</option>
+                                        <option value="{{ $category->id }}" {{ (old('category_id') == $category->id) ? 'selected' : '' }}>{{ $category->name }}</option>
                                     @endforeach
                                 </select>
                             </div>
