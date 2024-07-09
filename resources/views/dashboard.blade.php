ec03f491095b0ca5dc2acc04cc7785e32fe64831
Alan Tucker <alan.tucker@gmail.com>
2024-07-09 18:12:17 +0100

    Update dashboard view to handle empty tickets
    The dashboard view in the application has been updated to verify if there are no tickets available. If there are none, a message indicating this is displayed. Instead of the view crashing or displaying an empty table, this provides a better user experience.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,56 +12,62 @@
                     <div class="px-4 sm:px-6 lg:px-8">
                         <div class="sm:flex sm:items-center">
                             <div class="sm:flex-auto">
-                                <h2 class="text-xl font-semibold leading-6 text-gray-900">My Open Tickets</h2>
+                                <h2 class="text-xl font-semibold leading-6 text-gray-900">My Tickets</h2>
                             </div>
                             <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
                                 <a href="{{ route('ticket.create') }}" class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">New Ticket</a>
                             </div>
                         </div>
                         <div class="-mx-4 mt-8 sm:-mx-0">
-                            <table class="min-w-full divide-y divide-gray-300">
-                                <thead>
-                                <tr>
-                                    <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">
-                                        Date<span class="visible lg:invisible"> / Priority</span>
-                                    </th>
-                                    <th scope="col" class="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell">Priority</th>
-                                    <th scope="col" class="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 sm:table-cell">Subject</th>
-                                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Category</th>
-                                    <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-0">
-                                        <span class="sr-only">Edit</span>
-                                    </th>
-                                </tr>
-                                </thead>
-                                <tbody class="divide-y divide-gray-200 bg-white">
-                                @foreach($tickets as $ticket)
-                                <tr>
-                                    <td class="w-full max-w-0 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:w-auto sm:max-w-none sm:pl-0">
-                                        {{ $ticket->created_at->format("d/m/Y") }}
-                                        <dl class="font-normal lg:hidden">
-                                            <dt class="sr-only">Priority</dt>
-                                            <dd class="mt-1 truncate text-gray-700">
+
+                            @if(count($tickets) === 0)
+                                <p>You have no tickets</p>
+                            @else
+                                <table class="min-w-full divide-y divide-gray-300">
+                                    <thead>
+                                    <tr>
+                                        <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">
+                                            Date<span class="visible lg:invisible"> / Priority</span>
+                                        </th>
+                                        <th scope="col" class="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell">Priority</th>
+                                        <th scope="col" class="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 sm:table-cell">Subject</th>
+                                        <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Category</th>
+                                        <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-0">
+                                            <span class="sr-only">Edit</span>
+                                        </th>
+                                    </tr>
+                                    </thead>
+                                    <tbody class="divide-y divide-gray-200 bg-white">
+                                    @foreach($tickets as $ticket)
+                                        <tr>
+                                            <td class="w-full max-w-0 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:w-auto sm:max-w-none sm:pl-0">
+                                                {{ $ticket->created_at->format("d/m/Y") }}
+                                                <dl class="font-normal lg:hidden">
+                                                    <dt class="sr-only">Priority</dt>
+                                                    <dd class="mt-1 truncate text-gray-700">
+                                                        <x-priority-badge :number="$ticket->priority" />
+                                                    </dd>
+                                                    <dt class="sr-only sm:hidden">Subject</dt>
+                                                    <dd class="mt-1 truncate text-gray-500 sm:hidden">{{ $ticket->subject }}</dd>
+                                                </dl>
+                                            </td>
+                                            <td class="hidden px-3 py-4 text-sm text-gray-500 lg:table-cell">
                                                 <x-priority-badge :number="$ticket->priority" />
-                                            </dd>
-                                            <dt class="sr-only sm:hidden">Subject</dt>
-                                            <dd class="mt-1 truncate text-gray-500 sm:hidden">{{ $ticket->subject }}</dd>
-                                        </dl>
-                                    </td>
-                                    <td class="hidden px-3 py-4 text-sm text-gray-500 lg:table-cell">
-                                        <x-priority-badge :number="$ticket->priority" />
-                                    </td>
-                                    <td class="hidden px-3 py-4 text-sm text-gray-500 sm:table-cell">{{ $ticket->subject }}</td>
-                                    <td class="px-3 py-4 text-sm text-gray-500">{{ $ticket->category->name }}</td>
-                                    <td class="py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
-                                        <a href="{{ route('ticket.show', $ticket->id) }}" class="text-indigo-600 hover:text-indigo-900">View<span class="sr-only">, Lindsay Walton</span></a>
-                                    </td>
-                                </tr>
-                                @endforeach
-                                </tbody>
-                            </table>
-                            <div class="mt-6">
-                                {{ $tickets->links() }}
-                            </div>
+                                            </td>
+                                            <td class="hidden px-3 py-4 text-sm text-gray-500 sm:table-cell">{{ $ticket->subject }}</td>
+                                            <td class="px-3 py-4 text-sm text-gray-500">{{ $ticket->category->name }}</td>
+                                            <td class="py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+                                                <a href="{{ route('ticket.show', $ticket->id) }}" class="text-indigo-600 hover:text-indigo-900">View<span class="sr-only">, Lindsay Walton</span></a>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                                <div class="mt-6">
+                                    {{ $tickets->links() }}
+                                </div>
+                            @endif
+
                         </div>
                     </div>
                 </div>
